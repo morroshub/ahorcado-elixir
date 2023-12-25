9bdfd6a2b02f4b74e3a6369af6dbc7ce82fc8207
morroshub <lgmorrone@icloud.com>
2023-12-25 19:59:26 -0300

    se cambio la estructura de dato a mapset al archivo state.ex, se adaptara toda la app con la misma estructura.

--- lib/hangman/state.ex
+++ lib/hangman/state.ex
@@ -2,8 +2,9 @@ defmodule Hangman.State do
 
   def new(word) do
     word = String.downcase(word)
+    goal = word |> String.graphemes() |> MapSet.new
 
-    state = %{word: word, misses: [], matches: [], limit: 4, mask: "_", completed?: false}
+    %{word: word, goal: goal, misses: Mapset.new(), matches: Mapset.new(), limit: 4, mask: "_", completed?: false}
   end
 
 end
